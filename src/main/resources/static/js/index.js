31d0c706b28d29b7baeb46829c03f73a27fdceca
dhmk47 <dhmk47@naver.com>
2022-07-15 16:22:44 +0900

    자바스크립트 기능 구현중

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,47 @@
-/**
- * 
- */
+// header
+const headerNavItems = document.querySelectorAll(".header-nav-box li");
+const productDtlMenu = document.querySelector(".farm-product-dtl-menu");
+const boardDtlMenu = document.querySelector(".board-dtl-menu");
+
+
+let signinFlag = false;
+
+let productMenuFlag = false;
+let boardMenuFlag = false;
+
+headerNavItems[2].onclick = () => {
+    if(signinFlag) {
+
+    }else {
+        alert("먼저 로그인을 진행해 주세요.")
+    }
+}
+
+headerNavItems[0].onclick = () => {
+    if(productMenuFlag){
+        $(productDtlMenu).fadeOut(200);
+        productMenuFlag = false;
+    }else {
+        if(boardMenuFlag){
+            boardDtlMenu.click();
+        }
+        $(productDtlMenu).fadeIn(200);
+        productDtlMenu.style.display = "flex";
+        productMenuFlag = true;
+    }
+    
+}
+
+headerNavItems[1].onclick = () => {
+    if(boardMenuFlag){
+        $(boardDtlMenu).fadeOut(200);
+        boardMenuFlag = false;
+    }else {
+        if(productDtlMenu){
+            headerNavItems[0].click();
+        }
+        $(boardDtlMenu).fadeIn(200);
+        boardDtlMenu.style.display = "flex";
+        boardMenuFlag = true;
+    }
+}
