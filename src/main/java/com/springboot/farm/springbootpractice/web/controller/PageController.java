aafb29ca440cfe26a27aa66e5efdad4e38f46495
dhmk47 <dhmk47@naver.com>
2022-07-18 16:32:39 +0900

    signup page

--- src/main/java/com/springboot/farm/springbootpractice/web/controller/PageController.java
+++ src/main/java/com/springboot/farm/springbootpractice/web/controller/PageController.java
@@ -2,6 +2,7 @@ package com.springboot.farm.springbootpractice.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PageController {
@@ -9,5 +10,10 @@ public class PageController {
 	@GetMapping({"/", "/index"})
 	public String getMainPage() {
 		return "index";
+	}
+	
+	@GetMapping("/signup")
+	public String goSignupPage() {
+		return "signup";
 	}
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,6 +2,9 @@
 const headerNavItems = document.querySelectorAll(".header-nav-box li");
 const productDtlMenu = document.querySelector(".farm-product-dtl-menu");
 const boardDtlMenu = document.querySelector(".board-dtl-menu");
+//main
+const loginInputItems = document.querySelectorAll(".login-box input");
+const loginBoxButtons = document.querySelectorAll(".signin-signup-button-box button");
 
 
 let signinFlag = false;
@@ -9,12 +12,10 @@ let signinFlag = false;
 let productMenuFlag = false;
 let boardMenuFlag = false;
 
-headerNavItems[2].onclick = () => {
-    if(signinFlag) {
+load();
 
-    }else {
-        alert("먼저 로그인을 진행해 주세요.")
-    }
+document.querySelector("header h1").onclick = () => {
+    location.href = "/index";
 }
 
 headerNavItems[0].onclick = () => {
@@ -23,7 +24,7 @@ headerNavItems[0].onclick = () => {
         productMenuFlag = false;
     }else {
         if(boardMenuFlag){
-            boardDtlMenu.click();
+            headerNavItems[1].click();
         }
         $(productDtlMenu).fadeIn(200);
         productDtlMenu.style.display = "flex";
@@ -37,11 +38,57 @@ headerNavItems[1].onclick = () => {
         $(boardDtlMenu).fadeOut(200);
         boardMenuFlag = false;
     }else {
-        if(productDtlMenu){
+        if(productMenuFlag){
             headerNavItems[0].click();
         }
         $(boardDtlMenu).fadeIn(200);
         boardDtlMenu.style.display = "flex";
         boardMenuFlag = true;
     }
+}
+
+headerNavItems[2].onclick = () => {
+    if(signinFlag) {
+
+    }else {
+        alert("먼저 로그인을 진행해 주세요.")
+    }
+}
+
+document.querySelector("main").onmouseover = () => {
+    if(productMenuFlag){
+        $(productDtlMenu).fadeOut(200);
+        productMenuFlag = false;
+    }else if(boardMenuFlag){
+        $(boardDtlMenu).fadeOut(200);
+        boardMenuFlag = false;
+    }
+}
+
+loginBoxButtons[0].onclick = () => {
+    for(let i = 0; i < loginInputItems.length; i++){
+        if(isEmpty(loginInputItems[i].value)){
+            alert((i == 0 ? "아이디를"
+            : "비밀번호를") + " 입력해 주세요.");
+            return;
+        }
+    }
+}
+
+loginBoxButtons[1].onclick = () => {
+    location.href = "/signup";
+}
+
+function load(){
+    if(signinFlag) {
+        loginBoxButtons[0].innerHTML = "로그아웃";
+        loginBoxButtons[1].innerHTML = "내정보 보기";
+    }else {
+        loginBoxButtons[0].innerHTML = "로그인";
+        loginBoxButtons[1].innerHTML = "회원가입";
+    }
+}
+
+function isEmpty(value) {
+    return value == null || value == undefined || value == "";
 }
