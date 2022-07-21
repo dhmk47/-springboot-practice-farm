e65d4ce0c99883faabaf0a934d160df4f7ecdeb9
dhmk47 <dhmk47@naver.com>
2022-07-21 13:25:29 +0900

    productManage layout 및 기능

--- src/main/resources/static/js/managementProduct.js
+++ src/main/resources/static/js/managementProduct.js
@@ -11,11 +11,23 @@ const productAdminmenu = document.querySelectorAll(".farm-product-dtl-menu span"
 const userDtlMenu = document.querySelector(".user-dtl-menu");
 const userDtlMenuItems = document.querySelectorAll(".user-dtl-menu span");
 
+// main
+const customButtons = document.querySelectorAll(".custom-button-box button");
+const inputItems = document.querySelectorAll("main input");
+const checkButton = document.querySelector(".r-flex button");
+const changeProductInfoBoxes = document.querySelectorAll(".show-product-info-box");
+
 // 게시판 구분 짓는 플래그
 let userMenuFlag = false;
 let productMenuFlag = false;
 let boardMenuFlag = false;
 
+
+// 삭제버튼이면 input이 숨겨지기 때문에 flag 만들어서 if문 만들기
+let removeFlag = false;
+
+// 수정버튼이면 div박스 보여야 하기 때문에 구분 짓는 flag
+let showDivFlag = false;
 
 $(userDtlMenu).fadeOut(0);
 
@@ -121,6 +133,67 @@ productAdminmenu[0].onclick = () => {   // 농산물 품목 전체 보기
 
 productAdminmenu[1].onclick = () => {   // 농산물 관리
     
+}
+
+// main
+
+customButtons[0].onclick = () => {
+    if(removeFlag) {
+        inputItems[1].style.display = "inline-block";
+        inputItems[2].style.display = "inline-block";
+        inputItems[3].style.display = "inline-block";
+        removeFlag = false;
+    }
+    if(showDivFlag){
+        hideProductDivBox();
+        showDivFlag = false;
+    }
+    inputItems[0].placeholder = "추가할 농선물 이름";
+    inputItems[1].placeholder = "추가할 농선물 가격";
+    inputItems[2].placeholder = "추가할 농선물 계절";
+    inputItems[3].placeholder = "추가할 농선물 재배 기간";
+
+    
+}
+
+customButtons[1].onclick = () => {
+    if(removeFlag) {
+        inputItems[1].style.display = "inline-block";
+        inputItems[2].style.display = "inline-block";
+        inputItems[3].style.display = "inline-block";
+        removeFlag = false;
+    }
+    if(!showDivFlag) {
+        hideProductDivBox();
+        showDivFlag = true;
+    }
+    
+    inputItems[0].placeholder = "수정할 농선물 이름";
+    inputItems[1].placeholder = "수정할 농선물 가격";
+    inputItems[2].placeholder = "수정할 농선물 계절";
+    inputItems[3].placeholder = "수정할 농선물 재배 기간";
+}
+
+customButtons[2].onclick = () => {
+    if(showDivFlag){
+        hideProductDivBox();
+        showDivFlag = false;
+    }
+    removeFlag = true;
+    inputItems[0].placeholder = "삭제할 농선물 이름";
+    inputItems[1].style.display = "none";
+    inputItems[2].style.display = "none";
+    inputItems[3].style.display = "none";
+}
+
+
+checkButton
+
+
+function hideProductDivBox() {
+    changeProductInfoBoxes.forEach(i => {
+        i.classList.toggle("hideAndShowProductDivBox");
+    })
 }
 
 function errorMessage(request, status, error) {
