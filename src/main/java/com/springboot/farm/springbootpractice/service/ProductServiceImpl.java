8d1a67bdf2564d9780a089e396bc036c0877ca3b
dhmk47 <dhmk47@naver.com>
2022-07-26 20:23:39 +0900

    ....

--- src/main/java/com/springboot/farm/springbootpractice/service/ProductServiceImpl.java
+++ src/main/java/com/springboot/farm/springbootpractice/service/ProductServiceImpl.java
@@ -106,7 +106,7 @@ public class ProductServiceImpl implements ProductService{
 	
 	@Override
 	public boolean updateUsersProduct(UpdateProductReqDto updateProductReqDto) throws Exception {
-		return productRepository.updateUserProduct(updateProductReqDto.toEntity()) > 0;
+		return productRepository.updateUserProduct(updateProductReqDto.toEntity()) > 0;	/////////////
 	}
 
 	@Override
